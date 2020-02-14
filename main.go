d5e4d54be3461526380eac7860f7a135666a5efb
silmin <silvestmint@gmail.com>
2020-02-14 17:07:43 +0900

    add: update template

--- main.go
+++ main.go
@@ -2,11 +2,45 @@ package main
 
 import (
     "os"
+    "fmt"
+    "log"
+
     "github.com/urfave/cli"
 )
 
 
 func main() {
-    app := cli.NewApp()
-    app.Run(os.Args)
+    app := &cli.App {
+
+        Name: "test",
+        Usage: "test CLI tool",
+        Version: "0.0.1",
+
+        Commands: []*cli.Command {
+            {
+                Name: "hoge",
+                Usage: "say hoge",
+
+                Flags: []cli.Flag {
+                    &cli.BoolFlag {
+                        Name: "cat",
+                        Aliases: []string{"c"},
+                        Usage: "Echo with cat",
+                    },
+                },
+
+                Action: func (context *cli.Context) error {
+                    fmt.Println("hoge")
+                    if context.Bool("cat") { fmt.Println("nyan !") }
+                    return nil
+                },
+            },
+        },
+    }
+
+    err := app.Run(os.Args)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
+
