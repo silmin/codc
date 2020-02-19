11c9b353c985ef1ccce618297f34cd4c3da800f4
silmin <silvestmint@gmail.com>
2020-02-20 03:35:02 +0900

    binary

---
+++ combine/areas.go
@@ -0,0 +1,15 @@
+package combine
+
+import (
+    "strings"
+
+    types "github.com/silmin/odc-combiner/typefile"
+)
+
+func Area(figure types.Figure, params []string) (types.Figure, error) {
+
+    newArea := types.Areas{Name: strings.Join(params, "-")}
+    figure.Areas = append(figure.Areas, newArea)
+
+    return figure, nil
+}
