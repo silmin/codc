401ce476b26d0798e95c21a7d1c7391ccc99d451
silmin <silvestmint@gmail.com>
2020-02-21 05:47:47 +0900

    add: combine CounterSummary

--- combine/areas.go
+++ combine/areas.go
@@ -11,8 +11,13 @@ func Area(figure types.Figure, names []string) (types.Figure, error) {
         Name:   strings.Join(names, "-"),
         Source: names,
     }
-
     figure.Areas = append(figure.Areas, newArea)
+
+    newSummary, err := combineSummary(figure.CounterSummary, names)
+    if err != nil {
+        return types.Figure{}, err
+    }
+    figure.CounterSummary = append(figure.CounterSummary, newSummary)
 
     return figure, nil
 }
